1a8cbaa9817f2b75e79385c0c778b6af1eef9685
khbminus <khbminus@gmail.com>
2022-12-03 08:36:00 +0100

    Day 3 of Advent of Kotlin

---
+++ src/main/kotlin/Day3.kt
@@ -0,0 +1,34 @@
+import java.io.File
+
+class Day3 {
+    private fun readInput() = File(INPUT_PATH).readLines()
+
+    private fun getPriority(x: Char) = when {
+        x.isLowerCase() -> x - 'a' + 1
+        x.isUpperCase() -> x - 'A' + 27
+        else -> error("Invalid symbol")
+    }
+
+    private fun getPriorityOfRucksack(s: String): Int {
+        val (first, second) = s.toList().twoParts().map { it.toSet() }
+        return first.intersect(second).sumOf { getPriority(it) }
+    }
+
+    fun part1(): Int = readInput().sumOf { getPriorityOfRucksack(it) }
+
+    fun part2(): Int = readInput()
+        .map { it.toSet() }
+        .chunked(3)
+        .sumOf {
+            val initial = ('a'..'z').toSet() + ('A'..'Z').toSet()
+            it.fold(initial) { acc, chars -> acc.intersect(chars) }.sumOf { char -> getPriority(char) }
+        }
+
+    private fun <T> List<T>.twoParts() = chunked(size / 2)
+
+    companion object {
+        const val INPUT_PATH = "day3.in"
+//        const val INPUT_PATH = "day3.in.exmp"
+
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,4 +5,7 @@ fun main(args: Array<String>) {
     println("---DAY 2---")
     println("Answer for part 1 is ${Day2().part1()}")
     println("Answer for part 2 is ${Day2().part2()}")
+    println("---DAY 3---")
+    println("Answer for part 1 is ${Day3().part1()}")
+    println("Answer for part 2 is ${Day3().part2()}")
 }
